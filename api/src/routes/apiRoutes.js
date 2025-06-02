79281ed82e4c70793685bbc789fa806a0ad3abb7
gabriel1324t <gabriel.ignacio.carrasco1325@gmail.com>
2025-06-02 11:30:56 -0300

    02/06

--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 const veriyfyJWT = require("../services/verifyJWT");
 
+//rotas userController
 router.post("/user", userController.createUser);
 router.get("/user", veriyfyJWT, userController.getAllUsers);
 router.get("/user", userController.getAllUsers);
@@ -12,13 +13,13 @@ router.post("/login", userController.loginUser);
 
 const organizadorController = require("../controllers/organizadorController");
 
+//rotas oorganizadorController
 router.post("/organizador", organizadorController.createOrganizador);
 router.get("/organizador", organizadorController.getAllOrganizadores);
 router.put("/organizador", organizadorController.updateOrganizador);
 router.delete("/organizador/:id", organizadorController.deleteOrganizador);
 
 const eventoController = require("../controllers/eventoController");
-const ingressoController = require("../controllers/ingressoController");
 
 //rotas eventoController
 router.post("/evento", eventoController.createEvento);
@@ -28,11 +29,19 @@ router.delete("/evento/:id", eventoController.deleteEvento);
 router.get("/evento/data", veriyfyJWT, eventoController.getEventosPorData);
 router.get("/evento/proximo", eventoController.getEventosdia);
 
+const ingressoController = require("../controllers/ingressoController");
+
 //rotas ingressoController
 router.post("/ingresso", ingressoController.createIngresso);
 router.get("/ingresso", ingressoController.getAllIngressos);
 router.put("/ingresso", ingressoController.updateIngresso);
 router.delete("/ingresso/:id", ingressoController.deleteIngresso);
 router.get("/ingresso/evento/:id", ingressoController.getByIdEvento);
+
+const compraController = require("../controllers/compraController");
+
+//rotas compraController
+router.post("/comprasimples", compraController.registrarCompraSimples);
+router.post("/compra", compraController.registrarCompraSimples)
 
 module.exports = router;
